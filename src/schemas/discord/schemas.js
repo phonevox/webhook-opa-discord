0416b4d7cd9805b8a9b79e0f5405c1b48ae27358
Rafael Rizzo <rafaelrizzo@rafaelbreschi4@gmail.com>
2025-01-16 17:23:17 -0300

    fix: nome do parametro alterado para url_opa no schema

--- src/schemas/discord/schemas.js
+++ src/schemas/discord/schemas.js
@@ -1,9 +1,9 @@
 export const sendMessageSchema = {
     body: {
         type: 'object',
-        required: ['nome_empresa', 'client_name', 'number_client', 'msg', 'url'],
+        required: ['nome_empresa', 'client_name', 'number_client', 'msg', 'url_opa'],
         properties: {
-            url: { type: 'string', minLength: 1, maxLength: 255 },
+            url_opa: { type: 'string', minLength: 1, maxLength: 255 },
             nome_empresa: { type: 'string', minLength: 1, maxLength: 200 },
             client_name: { type: 'string', minLength: 1, maxLength: 255 },
             number_client: { type: 'string', minLength: 1, maxLength: 255 },
